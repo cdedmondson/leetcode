136a211b13ff208834d261d6c8133a74351618a5
Cameron Edmondson <42309690+cdedmondson@users.noreply.github.com>
2021-05-08 18:56:13 -0700

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -1,3 +1,28 @@
+# Contains Duplicate
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        arrayLength = len(nums)
+        # Step 1: If array is empty or contains only one value return False
+        if(arrayLength == 0 or arrayLength == 1):
+            return False
+        # Step 2: Create empty dictionary 
+        dict = {}
+        # Step 3: Add first value to dictionary as 'key' and count as 'value'
+        dict[nums[0]] = 1
+        # Step 4: Loop through array
+        for i in range(1, arrayLength):
+            # If value is in dictionary increase count by 1
+            if(nums[i] in dict):
+                dict[nums[i]] = dict[nums[i]] + 1;
+                # If count is >= 2 return True
+                if(dict[nums[i]] >= 2):
+                    return True
+                
+            dict[nums[i]] = 1
+            
+        # Step 5: return False
+        return False
+
 # Valid Anagram
 class Solution:
     def isAnagram(self, s: str, t: str):
